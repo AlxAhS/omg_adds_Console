1d6dce85f90aeb1a2004416b0f1414f3279dd809
Alx.AhS <alex.ahusoto@gmail.com>
2022-11-01 13:40:16 -0500

    Menu fix

--- OMG_ADDS/Export_document.cs
+++ OMG_ADDS/Export_document.cs
@@ -1,15 +1,8 @@
-﻿using OMG_ADDS;
-using iText.Kernel.Pdf;
+﻿using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Draw;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
-using Org.BouncyCastle.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace OMG_ADDS
 {
--- OMG_ADDS/Menu.cs
+++ OMG_ADDS/Menu.cs
@@ -8,72 +8,85 @@ using System.Threading.Tasks;
 
     public class Menu
     {
-    public static string Date { get; set; }
-    public static int Bill_Number { get; set; }
-    public static string Client_Name { get; set; }
-    public static string Client_ID { get; set; }
-    public static string Product_Description { get; set; }
-    public static string Product_Value { get; set; }
+        public static string Date { get; set; }
+        public static int Bill_Number { get; set; }
+        public static string Client_Name { get; set; }
+        public static string Client_ID { get; set; }
+        public static string Product_Description { get; set; }
+        public static string Product_Value { get; set; }
 
-    Form form = new Form();
-    Export_document export = new Export_document();
+        Form form = new Form();
+        Export_document export = new Export_document();
 
         public void Home_menu()
         {
-            byte option = 0;
-            string i = "";
+                string option = "";
+                byte select = 0;
 
-             do
-             {
                 Console.Clear();
                 Console.WriteLine("--------------------------------------------------------------");
                 Console.WriteLine("\n\t\t OMG Imports Bill-Generator");
                 Console.WriteLine("\t\t\t\t\t\t   by: Alx.AhS");
                 Console.WriteLine("--------------------------------------------------------------");
                 Console.WriteLine("\n\t\t*** BILL GENERATOR MENU ***");
-                Console.WriteLine("\t\t--------- Options ---------");
-                Console.WriteLine("\t1. CLIENT NAME: {0}", Client_Name);
-                Console.WriteLine("\t2. CLIENT ID (NIT / CC): {0}", Client_ID);
-                Console.WriteLine("\t3. PRODUCT DESCRIPTION: {0}", Product_Description);
-                Console.WriteLine("\t4. PRODUCT VALUE: $ {0} COP", Product_Value);
-                Console.WriteLine("\t5. GENERATE PDF {0}", i);
-                Console.WriteLine("\t6. EXIT ");
-                Console.Write("\nINSERT OPTION: ");
-                option = Convert.ToByte(Console.ReadLine());
-       
-                 switch(option)
-                 {
-                    case 1:
-                        if (Client_Name == null) 
-                        {form.Generate_values();
-                        Date = form.Date;
-                        Bill_Number = form.Bill_Number;
-                        }
-                        Console.Write("1. CLIENT NAME: ");
-                        Client_Name = Console.ReadLine();
-                        break;
-                    case 2:
-                        Console.Write("2. CLIENT ID: ");
-                       Client_ID = Console.ReadLine();
-                        break;
-                    case 3:
-                        Console.Write("3. PRODUCT DESCRIPTION: ");
-                        Product_Description= Console.ReadLine();
-                        break;
-                    case 4:
-                        Console.Write("4. PRODUCT VALUE: $ ");
-                        Product_Value = Console.ReadLine();
-                        break;
-                    case 5:
-                    if (option == 5)
+
+                form.Generate_values();
+                Date = form.Date;
+                Bill_Number = form.Bill_Number;
+
+                Console.Write("\n1. CLIENT NAME: ");
+                Client_Name = Console.ReadLine();
+                Console.Write("2. CLIENT ID: ");
+                Client_ID = Console.ReadLine();
+                Console.Write("3.PRODUCT DESCRIPTION: ");
+                Product_Description = Console.ReadLine();
+                Console.Write("4.PRODUCT VALUE (COP): $ ");
+                Product_Value = Console.ReadLine();
+
+                Console.Write("CONTINUE? (Y/N): ");
+                option = Console.ReadLine();
+
+                while ((option != "Y") && (option != "y"))
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\t\t--SELECT THE OPTION FOR REPLACE--\n");
+                    Console.WriteLine("\t1. CLIENT NAME: {0}", Client_Name);
+                    Console.WriteLine("\t2. CLIENT ID (NIT / CC): {0}", Client_ID);
+                    Console.WriteLine("\t3. PRODUCT DESCRIPTION: {0}", Product_Description);
+                    Console.WriteLine("\t4. PRODUCT VALUE: $ {0} COP", Product_Value);
+                    Console.WriteLine("\t5. CREATE PDF FILE ");
+                    Console.Write("\nOPTION: ");
+                    select = Convert.ToByte(Console.ReadLine());
+
+                    switch (select)
                     {
-                        i = " ... (file created successfully in the directory C:\\) ";
+                        case 1:
+                            Console.Write("1. CLIENT NAME: ");
+                            Client_Name = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.Write("2. CLIENT ID: ");
+                            Client_ID = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.Write("3. PRODUCT DESCRIPTION: ");
+                            Product_Description = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.Write("4. PRODUCT VALUE: $ ");
+                            Product_Value = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.Write("CONTINUE? (Y/N): ");
+                            option = Console.ReadLine();
+                            break ;
                     }
+                    Console.Write("CONTINUE? (Y/N): ");
+                    option = Console.ReadLine();
+                }
+                    Console.WriteLine("\n(file created successfully in the directory C:\\) \n");
                     Export_document export = new Export_document();
                     export.Exportpdf();
-                    break;
-                 }
-             } while (option != 6);
         }
     }
 
